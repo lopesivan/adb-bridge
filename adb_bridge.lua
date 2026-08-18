5877550b0125fa4e88940d006d26cb24e657cee3
Mr. Ivan <lopesivan.ufrj@gmail.com>
2026-08-18 16:00:59 -0300

    fix: lazy-load all adb bridge commands

--- adb_bridge.lua
+++ adb_bridge.lua
@@ -1,35 +1,45 @@
 return {
-    {
-        "lopesivan/adb-bridge",
-        cmd    = {
-            "AdbShell",
-            "AdbDevices",
-            "AdbDevicesL",
-            "AdbState",
-            "AdbRoot",
-            "AdbRemount",
-            "AdbReboot",
-            "AdbTcpip"
-            "AdbTap",
-            "AdbSwipe",
-            "AdbKeyevent",
-            "AdbText",
-            "AdbStart",
-            "AdbPm",
-            "AdbSettings",
-            "AdbUiAutomator",
-            "AdbUninstall",
-        },
+  {
+    "lopesivan/adb-bridge",
+    branch = "cpp",
 
-        build  = "make",
-
-        opts   = {
-            lib_path = nil, -- nil = busca automática
-            serial   = nil, -- nil = usa único device conectado
-        },
-
-        config = function(_, opts)
-            require("adb-bridge").setup(opts)
-        end,
+    cmd = {
+      "AdbShell",
+      "AdbDevices",
+      "AdbDevicesL",
+      "AdbVersion",
+      "AdbState",
+      "AdbRoot",
+      "AdbUnroot",
+      "AdbRemount",
+      "AdbReboot",
+      "AdbTcpip",
+      "AdbForward",
+      "AdbPush",
+      "AdbPull",
+      "AdbInstall",
+      "AdbInput",
+      "AdbTap",
+      "AdbSwipe",
+      "AdbKeyevent",
+      "AdbText",
+      "AdbAm",
+      "AdbStart",
+      "AdbPm",
+      "AdbSettings",
+      "AdbUiAutomator",
+      "AdbUninstall",
     },
+
+    build = "make",
+
+    opts = {
+      lib_path = nil, -- nil = busca adb_bridge.so na raiz do plugin
+      serial = nil, -- nil = host:transport-any
+    },
+
+    config = function(_, opts)
+      require("adb-bridge").setup(opts)
+    end,
+  },
 }
